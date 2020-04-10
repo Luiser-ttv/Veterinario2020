fb8058cee3c6c777c339382b40266dc125050bc7
Luis Rioja Cáceres <7901045@alumnos.ufv.es>
2020-04-10 22:49:16 +0200

    Agregar archivos de proyecto.

---
+++ Veterinario2020/VentanaLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veterinario2020
+{
+    public partial class VentanaLogin : Form
+    {
+        
+        public VentanaLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("fraxito") && textBox2.Text.Contains("babyyoda"))
+            {
+                VentanaPrincipal ventana = new VentanaPrincipal(this);
+                ventana.Show();
+                this.Hide();
+            }
+            else 
+            {
+                label4.Text = "Usuario o contraseña incorrecto por favor intentalo de nuevo";
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ Veterinario2020/VentanaPrincipal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Veterinario2020
+{
+    public partial class VentanaPrincipal : Form
+    {
+        private VentanaLogin mainForm = null;
+        public VentanaPrincipal(Form callingForm)
+        {
+           
+            InitializeComponent();
+            
+        }
+        
+    }
+}
